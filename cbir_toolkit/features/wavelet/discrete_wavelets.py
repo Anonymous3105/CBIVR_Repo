9fbe56634d16367c4a6d6f8712bc5748280801be
Anonymous3105 <abhijeetambadekar057@gmail.com>
2019-04-10 17:21:01 +0530

    Fixed discrete wavelets.

--- cbir_toolkit/features/wavelet/discrete_wavelets.py
+++ cbir_toolkit/features/wavelet/discrete_wavelets.py
@@ -5,7 +5,7 @@ from scipy.special import entr
 
 
 def get_lastlevel_DWT(image, wavelet, lev=3):
-	return cv2.wavedec2(image, wavelet, level=lev)[:2]
+	return wavedec2(image, wavelet, level=lev)[:2]
 
 
 def extract_features(arr):
